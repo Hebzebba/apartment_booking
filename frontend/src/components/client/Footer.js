bf38d6cc517afcad7a49b8ed7ba1d5986b80c883
Hebzebba <kwashieseth34@gmail.com>
2022-08-18 21:21:26 +0000

    created backend branch

--- frontend/src/components/client/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="w-full h-24 bg-gray-700 flex items-center justify-center text-white">
-      <p>copyright &copy; {new Date().getFullYear()} || All right reserved</p>
-    </div>
-  );
-};
-
-export default Footer;
--- frontend/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import adsReducer from "./slice/adsSlice";
-
-export const store = configureStore({
-  reducer: { ads_data: adsReducer },
-});
